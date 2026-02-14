9fb0ef7e2df92d08197adc8637c44665b3972e58
honzarozek101 <honzarozek101@seznam.cz>
2026-02-14 12:58:09 +0100

    Update juno.config.ts

--- juno.config.ts
+++ juno.config.ts
@@ -3,20 +3,8 @@ import { defineConfig } from "@junobuild/config";
 export default defineConfig({
   satellite: {
     ids: {
-      production: "tnsgl-4qaaa-aaaal-asw7q-cai"  // Tvůj satellite ID
+      production: "tnsgl-4qaaa-aaaal-asw7q-cai"  // Tvůj satellite ID z Juno Console
     },
-    source: "build",  // Nahraje z build/ složky (index.html + JS/CSS)
-    predeploy: ["npm run build"]  // Spustí build
-  },
-  automation: {
-    github: {
-      repositories: [
-        {
-          owner: "honzarozek101",  // Tvůj username
-          name: "www",  // Název repo
-          refs: ["main"]
-        }
-      ]
-    }
+    source: "build"  // Nahraje soubory z build/ složky (index.html + JS/CSS)
   }
 });
